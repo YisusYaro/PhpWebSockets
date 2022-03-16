0f6c74ec8187de17bd52361fd66fe015793fec33
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2022-03-15 21:11:44 -0600

    feat: add timeout

--- www/public/index.php
+++ www/public/index.php
@@ -2,10 +2,11 @@
 echo '<body>';
 echo '<h1>Hello</h1>';
 echo '<script>';
-echo "const conn = new WebSocket('wss://ws.localhost');";
+echo "const conn = new WebSocket('ws://ws.localhost');";
 echo 'conn.onopen = function (e) {';
 echo 'console.log("Connection established!");';
 echo '};';
+echo 'conn.onopen();';
 echo 'conn.onmessage = function (e) {';
 echo 'console.log(e.data);';
 echo '};';
